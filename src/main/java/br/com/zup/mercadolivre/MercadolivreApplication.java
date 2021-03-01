32181a3cbc43fe028df4c5f619fe02090c8403cb
Dev-JeanSantos <jeancbsan@gmail.com>
2021-03-01 14:57:28 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/zup/mercadolivre/MercadolivreApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.mercadolivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadolivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadolivreApplication.class, args);
+	}
+
+}
